d5d91e9efcda6fa08a7a71246c3c6964d02dda4d
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-04-02 14:13:30 +0200

    started adding canvas draw

---
+++ README.md
@@ -0,0 +1,47 @@
+# Project's name
+Thor's Conquest!
+
+
+## Description
+Thor needs to get to Mjolnir/Stormbreaker without getting hit by Loki's knives preventing it.
+Getting hit would result in game-over. Get to the hammer to WIN!!!
+
+
+## MVP
+Use arrowkeys to move left and right to move throught the hills.
+
+
+## Backlog
+Adding stairs? + up/down movement.
+Adding 2nd level? + difficulty/lives
+
+## Data structure
+
+
+
+
+## States y States Transitions
+Definition of the different states and their transition (transition functions)
+
+- splashScreen
+- gameScreen
+- gameoverScreen
+- winScreen
+
+
+## Task
+
+
+
+## Additional Links
+
+
+### Trello
+[Link url](https://trello.com)
+
+
+### Slides
+[Link Slides.com](http://slides.com)
+
+
+
---
+++ index.js
@@ -0,0 +1,71 @@
+let canvas = document.getElementById('myCanvas');
+let ctx = canvas.getContext('2d');
+canvas.style.border = '5px solid brown';
+canvas.style.backgroundColor = '#D6B471'
+
+
+
+
+
+let mjolnirhq = new Image();
+mjolnirhq.src = './img/mjolnir-hq.png'
+
+let thorhq = new Image();
+thorhq.src = './img/thor-hq.png'
+
+let lokihq = new Image();
+lokihq.src = './img/loki.png'
+
+
+
+
+
+
+
+function draw(){
+   
+    
+    ctx.drawImage(mjolnirhq, 450, 20, mjolnirhq.width/25, mjolnirhq.height/25)
+    
+
+    //floor level 0 (l to r)
+    ctx.beginPath()
+    ctx.fillStyle = '#a86832'
+    ctx.strokeStyle = 'brown'
+    ctx.moveTo(0, canvas.height)
+    ctx.lineTo(canvas.width +10, canvas.height - 150)
+    ctx.lineTo(canvas.width,canvas.height)
+    ctx.lineTo(0, canvas.height)
+    ctx.fill()
+    ctx.stroke()
+    ctx.closePath()
+
+
+    //floor level 1 (r to l)
+    ctx.beginPath()
+    ctx.fillStyle = '#a86832'
+    ctx.strokeStyle = 'brown'
+    ctx.moveTo(canvas.width-180, canvas.height-200)
+    ctx.lineTo(0, canvas.height - 350)
+    ctx.lineTo(0, canvas.heigth - 200)
+    ctx.lineTo(canvas.width-180, canvas.height-200)
+    ctx.fill()
+    ctx.stroke()
+    ctx.closePath()
+
+
+
+
+    ctx.drawImage(thorhq, 25, 550, thorhq.width/15, thorhq.height/15)
+    ctx.drawImage(lokihq, 750, 0, lokihq.width/1.5, lokihq.height/1.5)
+    
+   
+}
+
+
+
+
+
+window.addEventListener('load', () => {
+    draw()
+})
